cb9c8373352a2853b8c9648e8b7a9fcd2ef7a21c
Vivian <vivian.vimonthip@gmail.com>
2026-07-15 15:12:37 +0200

    Waiting for Room

--- src/HotelBooking/Hotel.java
+++ src/HotelBooking/Hotel.java
@@ -1,32 +1,63 @@
 package HotelBooking;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Hotel {
+    // ==== ATTRIBUTES (Data the Hotel Stores) ======
 
-    private ArrayList<Reservation> reservations;    // List of all bookings
+    // This list stores all the rooms in the hotel
+    // waiting for room class
+
+    // This list stores all the reservations that have been made
+    // every time someone books a room, we add the Reservation object here
+    private ArrayList<Reservation> reservations;
+
     private GuestManager guestManager;
 
-    // Constructor
+
+    // ====== CONSTRUCTOR =========
     public Hotel() {
+        // create the 2 lists when a Hotel object is created
+        // this.rooms = new ArrayList<>();
         this.reservations = new ArrayList<>();
-        this.guestManager = new GuestManager();     //
+        this.guestManager = new GuestManager();
     }
 
-    public Reservation makeReservation(int guestId, int roomNumber, boolean breakfastIncluded) {
+    // ===== ROOM MANAGEMENT METHODS (Randall) =========
 
-        // Find guest using GuestManager
+    // ====== RESERVATION / BOOKING METHODS (Vivian) =======
+
+    public Reservation makeReservation(int guestId, boolean breakfastIncluded) {
+
+        // Find the guest using GuestManager
         Guest guest = guestManager.findGuestById(guestId);
         if (guest == null) {
-            System.out.println("Guest not found.");
+            System.out.println("Guest not found with ID: " + guestId);
             return null;
         }
 
-        // Find the room
-        // Waiting for Room class
+        // Find room
+        // waiting for room class
 
-        // Make the reservation
-        int newReservationId = reservations.size() + 1;
-        Reservation newReservation =///////
+        // Make a new reservation
+        int newId = reservations.size() + 1;
+        Reservation newReservation = new Reservation(newId, guest, breakfastIncluded);
+
+        // add reservation to the list
+        reservations.add(newReservation);
+
+        // mark room as booked
+        // waiting for room class
+
+        return newReservation;
+    }
+
+    public boolean cancelReservation(int reservationId) {
+        for (Reservation res : reservations) {
+            if (res.getReservationId() == reservationId) {
+                ////////
+            }
+        }
     }
 }
