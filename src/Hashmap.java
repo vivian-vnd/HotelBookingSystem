19fa14fb84146ebecbf693574dc52fd00e3e6a03
Vivian <vivian.vimonthip@gmail.com>
2026-07-15 12:22:24 +0200

    Updated

--- src/Hashmap.java
+++
@@ -1,8 +0,0 @@
-public class Hashmap {
-    public static void main(String[] args) {
-
-        // Hash Map
-        System.out.println("\n===== Prices (Hash map) =====");
-        
-    }
-}
--- src/test.java
+++
@@ -1,5 +0,0 @@
-public class test {
-    public static void main(String[] args) {
-        System.out.println("Can we make Brigadeiros tomorrow :)");
-    }
-}
