e0ab94c32fb106c28888b4ae51c322e4be73fd42
Vivian <vivian.vimonthip@gmail.com>
2026-07-15 14:47:20 +0200

    Updated

--- src/HotelBooking/Hotel.java
+++ src/HotelBooking/Hotel.java
@@ -1,5 +1,32 @@
 package HotelBooking;
 
+import java.util.ArrayList;
+
 public class Hotel {
 
+    private ArrayList<Reservation> reservations;    // List of all bookings
+    private GuestManager guestManager;
+
+    // Constructor
+    public Hotel() {
+        this.reservations = new ArrayList<>();
+        this.guestManager = new GuestManager();     //
+    }
+
+    public Reservation makeReservation(int guestId, int roomNumber, boolean breakfastIncluded) {
+
+        // Find guest using GuestManager
+        Guest guest = guestManager.findGuestById(guestId);
+        if (guest == null) {
+            System.out.println("Guest not found.");
+            return null;
+        }
+
+        // Find the room
+        // Waiting for Room class
+
+        // Make the reservation
+        int newReservationId = reservations.size() + 1;
+        Reservation newReservation =///////
+    }
 }
