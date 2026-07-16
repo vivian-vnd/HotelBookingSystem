025daa1e7444d2fe0b7001e81d59920840a75cd7
Vivian <vivian.vimonthip@gmail.com>
2026-07-16 19:26:46 +0200

    Updated

--- src/HotelBooking/Reservation.java
+++ src/HotelBooking/Reservation.java
@@ -1,61 +1,64 @@
 package HotelBooking;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
 public class Reservation {
     // === Attributes ===
     private int reservationId;
     private Guest guest;
     private Room room;
-    // private LocalDate checkInDate
-    // private LocalDate checkOutDate
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
     private boolean breakfastIncluded;
     private String status;
 
     // === Constructor ===
-    public Reservation(int reservationId, boolean breakfastIncluded, String status, Guest guest, Room room) {
+    public Reservation(int reservationId, Room room, LocalDate checkInDate, LocalDate checkOutDate, boolean breakfastIncluded, String status) {
         this.reservationId = reservationId;
         this.room = room;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
         this.breakfastIncluded = breakfastIncluded;
         this.guest = guest;
         this.status = "Active";     // Default staus when booking is made
     }
 
-    public Reservation(int newId, Guest guest, boolean breakfastIncluded, Room room) {
-        this.reservationId = newId;
-        this.guest = guest;
-        this.room = room;
-        this.breakfastIncluded = breakfastIncluded;
-        this.status = "Active";
-    }
-
-
     // === Getters ===
     public int getReservationId() {
         return reservationId;
     }
-
-    public boolean isBreakfastIncluded() {
-        return breakfastIncluded;
-    }
-
     public Guest getGuest() {
         return guest;
     }
-
+    public Room getRoom() {
+        return room;
+    }
+    public LocalDate getCheckInDate() {
+        return checkInDate;
+    }
+    public LocalDate getCheckOutDate() {
+        return checkOutDate;
+    }
+    public boolean isBreakfastIncluded() {
+        return breakfastIncluded;
+    }
     public String getStatus() {
         return status;
     }
 
-    public Room getRoom() {
-        return room;
-    }
+
 
     // === Methods ===
 
-    // Calculates how many nights the guest is staying
+    // Calculates how many nights the guest is staying (needs changes)
+    public int getNumberOfNights() {
+        return (int) ChronoUnit.DAYS.between(checkInDate, checkOutDate);
+    }
 
     // Get a summary of the reservation (printing)
     public String getReservationDetails() {
-        return "Reservation ID: " + reservationId + "\nGuest: " + guest.getName() + "\nRoom: " + room.getRoomNumber() + " (" + room.getType() + ")" + "\nBreakfast Included: " + (breakfastIncluded ? "Yes" : "No") + "\nStatus: " + status;
+        return "Reservation ID: " + reservationId + "\nGuest: " + guest.getName() + "\nRoom: " + room.getRoomNumber() + " (" + room.getType() + ")" + "\nCheck-in: " + checkInDate + "\nCheck-out: "+ checkOutDate + "\nBreakfast Included: " + (breakfastIncluded ? "Yes" : "No") + "\nStatus: " + status;
     }
 
     // Cancel the reservation
