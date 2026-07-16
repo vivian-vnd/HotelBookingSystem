d91565c843a39b856b83a93da449a0125c43e2c4
Vivian <vivian.vimonthip@gmail.com>
2026-07-16 12:36:35 +0200

    Updated

--- src/HotelBooking/Hotel.java
+++ src/HotelBooking/Hotel.java
@@ -7,7 +7,7 @@ public class Hotel {
     // ==== ATTRIBUTES (Data the Hotel Stores) ======
 
     // This list stores all the rooms in the hotel
-    // waiting for room class
+    private ArrayList<Room> rooms;
 
     // This list stores all the reservations that have been made
     // every time someone books a room, we add the Reservation object here
@@ -19,16 +19,27 @@ public class Hotel {
     // ====== CONSTRUCTOR =========
     public Hotel() {
         // create the 2 lists when a Hotel object is created
-        // this.rooms = new ArrayList<>();
+        this.rooms = new ArrayList<>();
         this.reservations = new ArrayList<>();
         this.guestManager = new GuestManager();
+    }
+
+    // ==== ROOM HELPER METHOD ======
+    public Room findRoomByNumber(int roomNumber) {
+        for (Room room : rooms) {
+            if (room.getRoomNumber() == roomNumber) {
+                return room;
+            }
+        }
+        return null;
     }
 
     // ===== ROOM MANAGEMENT METHODS (Randall) =========
 
     // ====== RESERVATION / BOOKING METHODS (Vivian) =======
 
-    public Reservation makeReservation(int guestId, boolean breakfastIncluded) {
+    public Reservation makeReservation(int guestId, int roomNumber,boolean breakfastIncluded) {
+
 
         // Find the guest using GuestManager
         Guest guest = guestManager.findGuestById(guestId);
@@ -38,7 +49,16 @@ public class Hotel {
         }
 
         // Find room
-        // waiting for room class
+        Room room = findRoomByNumber(roomNumber);
+        if (room == null) {
+            System.out.println("Room not found with ID: " + roomNumber);
+            return null;
+        }
+
+        if (!rooms.isAvailable()) {
+            System.out.println("Room is already booked for guest with ID: " + guestId);
+            return null;
+        }
 
         // Make a new reservation
         int newId = reservations.size() + 1;
@@ -48,16 +68,20 @@ public class Hotel {
         reservations.add(newReservation);
 
         // mark room as booked
-        // waiting for room class
+        rooms.bookRoom();
 
         return newReservation;
     }
 
     public boolean cancelReservation(int reservationId) {
         for (Reservation res : reservations) {
-            if (res.getReservationId() == reservationId) {
-                ////////
+            if (res.getReservationId() == reservationId && res.isActive()) {
+                res.cancelReservation();        // Change status to Cancelled
+                res.getRoom().releaseRoom();    // Free the room
+                return true;
             }
         }
+        System.out.println("Reservation not found or already cancelled");
+        return false;
     }
 }
