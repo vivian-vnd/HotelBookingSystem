f1dc8641b1960a043c0c0b51b0ad30d61b85e958
Vivian <vivian.vimonthip@gmail.com>
2026-07-18 22:32:16 +0200

    Basic structure Main class

--- src/HotelBooking/Main.java
+++ src/HotelBooking/Main.java
@@ -1,8 +1,17 @@
 package HotelBooking;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Hotel hotel = new Hotel();      // Create Hotel object
+        Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            // show menu
+            // Get user choice
+            // Call appropriate Hotel based on choice
+        }
     }
 }
